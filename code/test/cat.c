08cd77613deea66d907b94f24f414f3600de0c0c
sliceoflif3 <yunomiclover@gmail.com>
2023-03-16 15:37:57 +0700

    finish cat

--- code/test/cat.c
+++ code/test/cat.c
@@ -1,11 +1,21 @@
 #include "syscall.h"
-// #include "copyright.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    OpenFileId fd = Open("text.txt", 0);
+    char *filename, buffer[101];
+    OpenFileId fd;
 
-    char buffer[101];
+    if (argc < 2)
+    {
+        PrintString("Usage: cat <filename>");
+        Halt();
+    }
+
+    filename = argv[1];
+
+    fd = Open(filename, 0);
+
+    // char buffer[101];
     Read(buffer, 100, fd);
     PrintString(buffer);
 
